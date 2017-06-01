e045067eb51072b254deb0cd6946e3956e2279bc
Matt Kulka <mkulka@parchment.com>
2017-06-01 15:38:37 -0700

    support chef 13

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ version          '0.1.3'
 source_url 'https://github.com/mattlqx/cookbook-efs'
 issues_url 'https://github.com/mattlqx/cookbook-efs/issues'
 
+chef_version '>= 12'
+
 supports 'ubuntu'
 supports 'centos'
 supports 'rhel'
